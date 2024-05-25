30eea6f7640c805c5541a95d21b80b193e1e3e77
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-05-25 14:22:38 -0600

    tab bar shopping list first part

--- TabBarShoppingList/ViewController.swift
+++ TabBarShoppingList/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView:UITableView!
+    
+    //var arrayShoppingList = [String]()
+    var arrayShoppingList = ["uno", "dos", "tres"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        //tableView.delegate = self
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var numberRows = 0
+        if section == 0 {
+            numberRows = arrayShoppingList.count
+        }
+        return numberRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CustomTableViewCell
+        cell?.cellTextLabel.text = arrayShoppingList[indexPath.row]
+        
+        return cell!
+    }
 }
 
